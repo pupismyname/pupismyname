6338e27f8e1445bb0d494e29a7907ae7b54af99d
John Brooks <john.brooks@ookla.com>
2022-11-17 16:34:41 -0600

    favicon, postcss, logo

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,14 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
 const { DateTime } = require("luxon");
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const processStyles = require('./lib/processStyles.js');
 
-module.exports = (function (eleventyConfig) {
+module.exports = ((eleventyConfig) => {
+
+  eleventyConfig.setWatchThrottleWaitTime(1000);
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPlugin(pluginRss);
@@ -10,13 +16,35 @@ module.exports = (function (eleventyConfig) {
   eleventyConfig.addWatchTarget('assets');
   eleventyConfig.addWatchTarget('styles');
 
-  eleventyConfig.addPassthroughCopy('styles');
-  eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addPassthroughCopy({ 'styles': '/s/styles' });
+  eleventyConfig.addPassthroughCopy({ 'assets': '/s' });
 
   eleventyConfig.addFilter('readableDateTime', (dateObj) => {
     const dt = DateTime.fromJSDate(dateObj, { zone: 'utc' }).setZone('America/Chicago');
     return dt.setLocale('en').toLocaleString(DateTime.DATETIME_FULL);
   });
+
+  eleventyConfig.addGlobalData('cacheBusters', async (eleventyConfig) => {
+    await processStyles();
+    return {
+      styles: await getFileHash('_site/s/styles/styles.min.css'),
+    }
+  });
+
+  async function getFileHash (hashPath) {
+    try {
+      if (!hashPath) return;
+      const fileBuffer = await fs.promises.readFile(path.resolve(hashPath));
+      const hashSum = crypto.createHash('sha256');
+      hashSum.update(fileBuffer);
+      const digest = hashSum.digest('hex');
+      return digest;
+    } catch (e) {
+      console.error('There was a problem creating cacheBusters.');
+      console.error(e);
+    }
+  }
+
 
   return {
     dir: {
