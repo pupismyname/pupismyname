7e6bd18d894461d9c3719f8851b1f8d5ce9b77ac
John Brooks <john.brooks@ookla.com>
2023-02-27 15:19:37 -0600

    Update design, update build organization, add image processing

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,30 @@
-const crypto = require('crypto');
-const fs = require('fs');
-const path = require('path');
-const { DateTime } = require("luxon");
-const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const processStyles = require('./lib/processStyles.js');
+const { DateTime } = require('luxon');
+const getFileHash = require('./lib/get-file-hash');
+const processStyles = require('./lib/process-styles.js');
+const eleventyNavigation = require('@11ty/eleventy-navigation');
+const eleventyPluginRss = require('@11ty/eleventy-plugin-rss');
+const eleventyFriendlyImages = require('./lib/eleventy-friendly-images');
 
 module.exports = ((eleventyConfig) => {
 
+  eleventyConfig.setServerOptions({
+    liveReload: false, // disable live reload so event listeners keep working
+  });
+
   eleventyConfig.setWatchThrottleWaitTime(1000);
 
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addPlugin(pluginRss);
-
+  eleventyConfig.addPlugin(eleventyNavigation);
+  eleventyConfig.addPlugin(eleventyPluginRss);
+  eleventyConfig.addPlugin(eleventyFriendlyImages, { selector: '.content img' });
   eleventyConfig.addWatchTarget('assets');
   eleventyConfig.addWatchTarget('styles');
 
-  eleventyConfig.addPassthroughCopy({ 'styles': '/s/styles' });
+  // eleventyConfig.addPassthroughCopy({ 'styles': '/s/styles' });
   eleventyConfig.addPassthroughCopy({ 'assets': '/s' });
+  eleventyConfig.addPassthroughCopy('**/*.png');
+  eleventyConfig.addPassthroughCopy('**/*.jpg');
+  eleventyConfig.addPassthroughCopy('**/*.gif');
+  eleventyConfig.addPassthroughCopy('**/*.webp');
 
   eleventyConfig.addFilter('metadataDateTime', (dateObj) => {
     const dt = DateTime.fromJSDate(dateObj, { zone: 'America/Chicago' }).setZone('utc');
@@ -29,26 +36,14 @@ module.exports = ((eleventyConfig) => {
     return dt.setLocale('en').toLocaleString(DateTime.DATETIME_FULL);
   });
 
+  // addWatchTarget on 'styles' triggers this, which processes the styles and returns the
+  // cachebusters for the templates
   eleventyConfig.addGlobalData('cacheBusters', async (eleventyConfig) => {
     await processStyles();
     return {
       styles: await getFileHash('_site/s/styles/styles.min.css'),
     }
   });
-
-  async function getFileHash (hashPath) {
-    try {
-      if (!hashPath) return;
-      const fileBuffer = await fs.promises.readFile(path.resolve(hashPath));
-      const hashSum = crypto.createHash('sha256');
-      hashSum.update(fileBuffer);
-      const digest = hashSum.digest('hex');
-      return digest;
-    } catch (e) {
-      console.error('There was a problem creating cacheBusters.');
-      console.error(e);
-    }
-  }
 
   return {
     dir: {
