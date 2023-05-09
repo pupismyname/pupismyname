6537cb0369328dcd2433f491107a87cb6d959eb6
John Brooks <john.brooks@ookla.com>
2023-05-09 13:04:56 -0500

    update css build process

--- content/_data/cacheBusters.js
+++ content/_data/cacheBusters.js
@@ -2,12 +2,6 @@ const getHash = require('../../lib/get-hash');
 
 // Generate hashes for assets to use for cache busting
 module.exports = async () => {
-  // Don't do this for dev.
-  if (process.env.ENV === 'development') {
-    return {
-      styles: 1,
-    };
-  }
   return {
     // Generate a hash based on the minified css
     styles: getHash.fromFile('_site/s/styles/styles.min.css'),
